b189069e944dd58f64bbb08dc03206296cbd2043
András <hajduandras486@gmail.com>
2023-04-08 16:18:10 +0200

    Added Student model, service, controller

--- src/main/java/com/SpringBootCourse/SpringBootCourseApplication.java
+++ src/main/java/com/SpringBootCourse/SpringBootCourseApplication.java
@@ -2,22 +2,12 @@ package com.SpringBootCourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringBootCourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCourseApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public List<String> hello() {
-		return List.of("Hello", "World");
 	}
 
 }
